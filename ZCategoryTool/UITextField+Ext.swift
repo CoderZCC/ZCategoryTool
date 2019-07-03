09cecf2f26c3fa5dffec2fc3f3b6c82933a1c2d2
zcc <coderzcca@gmail.com>
2019-07-03 17:18:46 +0800

    fix: -修复UITextField占位文字无法显示问题

--- ZCategoryTool/UITextField+Ext.swift
+++ ZCategoryTool/UITextField+Ext.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 extension UITextField {
-    
-    /// 占位文字颜色
+
+    /// 占位文字颜色, 先设置占位文字,在设置颜色,否则失效
     public var k_placeholderColor: UIColor? {
         set {
             k_setAssociatedObject(key: "k_placeholderColor", value: newValue)
+            if let color = newValue, let placeholder = self.placeholder {
+                let defaultFont = self.font ?? UIFont.systemFont(ofSize: 14.0)
+                let attributeStr = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : color, .font: defaultFont])
+                self.attributedPlaceholder = attributeStr
+            }
         }
         get { return k_getAssociatedObject(key: "k_placeholderColor") as? UIColor }
     }
@@ -31,23 +36,12 @@ extension UITextField {
             return k_getAssociatedObject(key: "kUITextFieldLimitTextLengthKey") as? Int
         }
     }
-    
+
     @objc private func _textFieldChangeAction(note: NSNotification) {
         guard let maxCount = self.k_limitTextLength else { return }
         let inputText: String = self.text ?? ""
         if inputText.count > maxCount && self.markedTextRange == nil {
             self.text = inputText.k_subText(to: maxCount - 1)
-        }
-    }
-    
-    // 重写方法
-    open override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        if let color = self.k_placeholderColor, let placeholder = self.placeholder, self.attributedPlaceholder == nil {
-            let defaultFont = self.font ?? UIFont.systemFont(ofSize: 14.0)
-            let attributeStr = NSAttributedString(string: placeholder, attributes: [NSAttributedString.Key.foregroundColor : color, .font: defaultFont])
-            self.attributedPlaceholder = attributeStr
         }
     }
     
--- ZCategoryToolDemo/ViewController.swift
+++ ZCategoryToolDemo/ViewController.swift
@@ -15,54 +15,21 @@ class ViewController: UIViewController {
         
         self.view.backgroundColor = UIColor.lightGray
         
-//        let btn = UIButton.init(type: UIButton.ButtonType.contactAdd)
-//        btn.sizeToFit()
-//        btn.center = self.view.center
-//        self.view.addSubview(btn)
-//        btn.addTarget(events: UIControl.Event.touchUpInside, inTarget: self) { (weakBtn, weakSelf) in
-//
-//            print(weakBtn)
-//            print(weakSelf)
-//            weakSelf.present(ViewController1(), animated: true, completion: nil)
-//        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.present(ViewController1(), animated: true, completion: nil)
+        let textField = UITextField(frame: CGRect(x: 0.0, y: 0.0, width: 100.0, height: 35.0))
+        textField.backgroundColor = UIColor.black
+        textField.center = self.view.center
+        textField.keyboardAppearance = .dark
+        textField.tintColor = UIColor.white
+        textField.textColor = UIColor.white
+        textField.font = UIFont.systemFont(ofSize: 16.0)
+        textField.returnKeyType = .search
+        textField.placeholder = "查找笔记"
+        textField.k_placeholderColor = UIColor.white
+        
+        self.view.addSubview(textField)
     }
     
     deinit {
         print("\(self)销毁了")
     }
 }
-
-class ViewController1: UIViewController {
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.view.backgroundColor = UIColor.darkGray
-        
-        
-        let imgV = UIImageView.init(image: UIImage.init(named: "img"))
-        imgV.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
-        imgV.center = self.view.center
-        self.view.addSubview(imgV)
-        imgV.addTapGesture(target: self) { (tap, weakSelf) in
-            
-            print(tap)
-            print(weakSelf)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-//        self.dismiss(animated: true, completion: nil)
-    }
-    
-    deinit {
-        print("\(self)销毁了")
-    }
-}
-
